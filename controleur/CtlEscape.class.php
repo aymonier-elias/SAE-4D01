0fe77ee627c6185453e6252fcddea9d7a31cbedb
TryR68 <trystan.roussel88@gmail.com>
2026-03-04 11:56:35 +0100

    modification routeur escape+reservation

---
+++ controleur/CtlEscape.class.php
@@ -0,0 +1,32 @@
+<?php
+require_once "modele/escape.class.php";
+require_once "vue/vue.class.php";
+
+
+// Classe chargée de la gestion des escapes
+
+
+class CtlEscape{
+
+    private $escape;
+
+    public function __construct() {
+        $this->escape = new Escape();
+    }
+
+
+    /*******************************************************
+Affichage de la liste des escapes dans la vue concernée
+  Entrée : 
+
+  Retour : 
+    
+*******************************************************/
+    public function escapes() {
+        $escapes = $this->article->getEscapes();
+        $vue = new Vue("Escapes"); // Instancie la vue appropriée
+        $vue->afficher(array("escapes" => $escapes)); 
+    }
+
+
+}
