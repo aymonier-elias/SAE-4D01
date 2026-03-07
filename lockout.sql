4043c9f227a626a155fa901855e74a9f35c8484d
TryR68 <trystan.roussel88@gmail.com>
2026-03-07 13:53:15 +0100

    ajout bdd

---
+++ lockout.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : sam. 07 mars 2026 à 13:52
+-- Version du serveur : 5.7.43
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `lockout`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acheter`
+--
+
+CREATE TABLE `acheter` (
+  `id_client` int(11) NOT NULL,
+  `id_version` int(11) NOT NULL,
+  `date` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `heure` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nb_participant` int(5) NOT NULL,
+  `reserver` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `id_avis` int(11) NOT NULL,
+  `date` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `commentaire` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `note` int(1) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `id_version` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `carte_cadeau`
+--
+
+CREATE TABLE `carte_cadeau` (
+  `id_carte` int(11) NOT NULL,
+  `valeur` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `validité` date NOT NULL,
+  `nb_escape` int(10) NOT NULL,
+  `id_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prénom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mail` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mdp` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `statut` int(5) NOT NULL,
+  `adresse` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code_postal` int(5) NOT NULL,
+  `ville` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `téléphone` int(17) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom`, `prénom`, `mail`, `mdp`, `statut`, `adresse`, `code_postal`, `ville`, `téléphone`) VALUES
+(1, 'R', 'Trystan', 'trystan@gmail.com', '$2y$10$z9/NEjogI2XQkyB76L8nJOwOVoDze7Pq4vVwN.IWsN0Rn/kO9SaeC', 2, '', 0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `escape_game`
+--
+
+CREATE TABLE `escape_game` (
+  `id_escape` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `longitude` int(20) NOT NULL,
+  `latitude` int(20) NOT NULL,
+  `nb_participants_max` int(5) NOT NULL,
+  `age_minimum` int(2) NOT NULL,
+  `ville` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tags` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `difficultés` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mettre_favoris_carte`
+--
+
+CREATE TABLE `mettre_favoris_carte` (
+  `id_carte` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mettre_favoris_version`
+--
+
+CREATE TABLE `mettre_favoris_version` (
+  `id_client` int(11) NOT NULL,
+  `id_version` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `version`
+--
+
+CREATE TABLE `version` (
+  `id_version` int(11) NOT NULL,
+  `durée` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prix` int(10) NOT NULL,
+  `id_escape` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id_avis`);
+
+--
+-- Index pour la table `carte_cadeau`
+--
+ALTER TABLE `carte_cadeau`
+  ADD PRIMARY KEY (`id_carte`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `escape_game`
+--
+ALTER TABLE `escape_game`
+  ADD PRIMARY KEY (`id_escape`);
+
+--
+-- Index pour la table `version`
+--
+ALTER TABLE `version`
+  ADD PRIMARY KEY (`id_version`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `id_avis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `carte_cadeau`
+--
+ALTER TABLE `carte_cadeau`
+  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `escape_game`
+--
+ALTER TABLE `escape_game`
+  MODIFY `id_escape` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `version`
+--
+ALTER TABLE `version`
+  MODIFY `id_version` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
