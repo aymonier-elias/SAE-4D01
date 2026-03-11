87d97302bd91c6c7776f5964b9a7b1208a9bb3af
JoelHH68 <joelhauptmann@gmail.com>
2026-03-11 12:00:05 +0100

    traduction footer

--- includes/default_config.php
+++ includes/default_config.php
@@ -18,9 +18,9 @@ $conf->nomSite = Config::NOMSITE;
 //Menu par défaut de la page
 $conf->menu = "
         <div class = 'menu'>
-                <a href='index.php?action=escapes'>Les missions</a>
-                <a href='#exp'>Le concept</a>
-                <a href='index.php?action=contact'>Contact</a>
+                <a href='index.php?action=escapes' data-i18n='header.missions'>Les missions</a>
+                <a href='#exp' data-i18n='header.concept'>Le concept</a>
+                <a href='index.php?action=contact' data-i18n='header.contact'>Contact</a>
                 <button class='btn_langue'><img src='img/svg/fr.svg' aria-expended='false'></button>
             <div class = 'important'>
                 <a href='index.php?action=connexion'>Se connecter</a>
@@ -33,9 +33,9 @@ $conf->menu = "
 //Menu connecté avec un compte utilisateur normal
 $conf->menu_connecte = "
     <div class = 'menu'>
-        <a href='index.php?action=escapes'>Les missions</a>
-        <a href='#exp'>Le concept</a>
-        <a href='index.php?action=contact'>Contact</a>
+        <a href='index.php?action=escapes' data-i18n='header.missions'>Les missions</a>
+        <a href='#exp' data-i18n='header.concept'>Le concept</a>
+        <a href='index.php?action=contact' data-i18n='header.contact'>Contact</a>
         <button class='btn_langue'><img src='img/svg/fr.svg' aria-expended='false'></button>
         <div class = 'important'>
             <a href='index.php?action=panier'><img src='img/svg/panier.svg'></a>
@@ -54,9 +54,9 @@ $conf->menu_admin = "
         <a href='index.php?action=contact'>Contact</a>
         <button class='btn_langue'><img src='img/svg/fr.svg' aria-expended='false'></button>
         <div class = 'admin-links'>
-            <a href='index.php?action=gestion_utilisateurs'>Gestion Utilisateurs</a>
-            <a href='index.php?action=gestion_commandes'>Gestion Commandes</a>
-            <a href='index.php?action=gestion_escapegame'>Gestion EscapeGame</a>
+            <a href='index.php?action=gestion_utilisateurs' data-i18n='header.gestion-ut'>Gestion Utilisateurs</a>
+            <a href='index.php?action=gestion_commandes' data-i18n='header.gestion-com'>Gestion Commandes</a>
+            <a href='index.php?action=gestion_escapegame' data-i18n='header.gestion-esc'>Gestion EscapeGame</a>
         </div>
 
         <div class = 'important'>
@@ -72,9 +72,9 @@ $conf->footer = '<nav class="footer-nav">
       <a href="#" class="logo"><img src="img/svg/Logo.svg" alt=""></a>
 
       <div class="nav-coordonees">
-        <h3>Coordonées</h3>
+        <h3 data-i18n="footer.coordonnees.titre">Coordonées</h3>
         <a href="#">
-          <img src="img/svg/adresse.svg" alt="">
+          <img src="img/svg/adresse.svg" alt="" data-i18n="footer.coordonnees.adresse">
           55 rue de Pfastatt, 68200 MULHOUSE
         </a>
         <a href="#">
@@ -90,21 +90,21 @@ $conf->footer = '<nav class="footer-nav">
       <span class="footer-separator"></span>
 
       <div class="nav-protocoles">
-        <h3>Protocoles</h3>
-        <a href="#">Les missions</a>
-        <a href="#">Le concept</a>
-        <a href="#">FAQ</a>
-        <a href="#">Conatct</a>
-        <a href="#">Entreprise</a>
+        <h3 data-i18n="footer.protocoles.titre">Protocoles</h3>
+        <a href="#" data-i18n="footer.protocoles.missions">Les missions</a>
+        <a href="#" data-i18n="footer.protocoles.concept">Le concept</a>
+        <a href="#" data-i18n="footer.protocoles.faq">FAQ</a>
+        <a href="#" data-i18n="footer.protocoles.contact">Conatct</a>
+        <a href="#" data-i18n="footer.protocoles.entreprise">Entreprise</a>
       </div>
 
       <span class="footer-separator"></span>
 
       <div class="nav-confidentialite">
-        <h3>Protocoles</h3>
-        <a href="#">Mentions Légales</a>
-        <a href="#">CGV</a>
-        <a href="#">Cookies</a>
+        <h3 data-i18n="footer.confidentialite.titre">Confidentialité</h3>
+        <a href="#" data-i18n="footer.confidentialite.mentions">Mentions Légales</a>
+        <a href="#" data-i18n="footer.confidentialite.cvg">CGV</a>
+        <a href="#" data-i18n="footer.confidentialite.cookies">Cookies</a>
       </div>
     </nav>
 
@@ -116,4 +116,4 @@ $conf->footer = '<nav class="footer-nav">
       <a href="#"><img src="img/svg/tiktok.svg" alt=""></a>
     </div>
 
-    <p class="legal">Propriété de lAgence Lock Out. Transmission de données cryptée. © 2026</p>';
+    <p class="legal" data-i18n="footer.copyright">Propriété de lAgence Lock Out. Transmission de données cryptée. © 2026</p>';
